c74dd0e413d8814f02c18e68d9471cac5a2bf42e
Phil Armstrong <lotsadollas@gmail.com>
2012-09-27 07:04:33 -0600

    add connection panel

---
+++ src/main/java/com/betadb/gui/connection/ConnectionInfo.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.betadb.gui.connection;
+
+/**
+ *
+ * @author Phil
+ */
+public class ConnectionInfo
+{
+
+	private String serverName;
+	private String userName;
+	private String domain;
+	private String instanceName;
+
+	public ConnectionInfo(String serverName, String userName, String domain, String instanceName)
+	{
+		this.serverName = serverName;
+		this.userName = userName;
+		this.domain = domain;
+		this.instanceName = instanceName;
+	}
+
+	public String getServerName()
+	{
+		return serverName;
+	}
+
+	public String getUserName()
+	{
+		return userName;
+	}
+
+	public String getDomain()
+	{
+		return domain;
+	}
+
+	public String getInstanceName()
+	{
+		return instanceName;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final ConnectionInfo other = (ConnectionInfo) obj;
+		if ((this.serverName == null) ? (other.serverName != null) : !this.serverName.equals(other.serverName))
+			return false;
+		if ((this.domain == null) ? (other.domain != null) : !this.domain.equals(other.domain))
+			return false;
+		if ((this.instanceName == null) ? (other.instanceName != null) : !this.instanceName.equals(other.instanceName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		int hash = 7;
+		return hash;
+	}
+
+	@Override
+	public String toString()
+	{
+		String instanceNameSuffix = instanceName == null || instanceName.isEmpty() ? "" : "\\"+instanceName;
+		return serverName + instanceNameSuffix;
+	}
+	
+	
+}
