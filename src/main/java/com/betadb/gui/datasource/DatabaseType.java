9bab056c261a79ca534ac23a21eca1058def0f38
Phil Armstrong <lotsadollas@gmail.com>
2013-06-20 17:17:51 -0600

    update for preliminary postgres support

---
+++ src/main/java/com/betadb/gui/datasource/DatabaseType.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.betadb.gui.datasource;
+
+/**
+ *
+ * @author parmstrong
+ */
+public enum DatabaseType
+{
+	MSSQL("MS Sql Server", "net.sourceforge.jtds.jdbc.Driver", "jdbc:jtds:sqlserver://"),
+	POSTGRES("Postgres", "org.postgresql.Driver", "jdbc:postgresql://");
+
+	private String displayName;
+	private String driverClassName;
+	private String url;
+
+	private DatabaseType(String displayName, String driverClassName, String url)
+	{
+		this.displayName = displayName;
+		this.driverClassName = driverClassName;
+		this.url = url;
+	}
+
+	@Override
+	public String toString()
+	{
+		return getDisplayName();
+	}
+
+	public String getDisplayName()
+	{
+		return displayName;
+	}
+
+	public String getDriverClassName()
+	{
+		return driverClassName;
+	}
+
+	public String getUrl()
+	{
+		return url;
+	}
+}
